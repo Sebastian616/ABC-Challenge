771de5bebead93234be1955ebe8b5f4b5e25a2f5
NatalyBarahona <nicolbarahona.6012@gmail.com>
2026-07-23 10:17:18 -0500

    boton vocales

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("hola mundo")
+const btnVocales = document.getElementById('btn-vocales');
+btnVocales.addEventListener('click', () => {
+  cards.forEach(card => {
+    if (card.dataset.tipo === 'vocal') {
+      card.style.display = 'block'; 
+    } else {
+      card.style.display = 'none';
+    }
+  });
+});
