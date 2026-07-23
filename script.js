9bf99d4606b17ace63ec8c3352cc088e797c46a5
SamuelVelez_G <samu2005vel@gmail.com>
2026-07-23 12:05:49 -0500

    g

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+const btnVocales = document.getElementById('btn-vocales');
+btnVocales.addEventListener('click', () => {
+    cards.forEach(card => {
+        if (card.dataset.tipo === 'vocal') {
+            card.style.display = 'block';
+        } else {
+            card.style.display = 'none';
+        }
+    })
+})
+//creamos la constante y entramos al div por id
+const card = document.getElementById("card");
+
+//accedemos a card y decimos que cuando se de clic entra a card, luego a la propiedad classlist y por ultimo activa toggle que provoca el giro al entrar al css
+card.addEventListener("click", () => {
+    card.classList.toggle("girar");
+})
+
+
+
+const boton = document.getElementById("botonTodas");
+
+boton.addEventListener("click", function () {
+    alert("¡Hola! Presionaste el botón.");
+});
 let letrasVistas = 0;
 
 function voltear(card) {
