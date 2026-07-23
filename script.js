cc06b9ca5f5f4a9c8480fef943667625af46f693
MishelleBohorquez <dmishellebohorquez@ucundinamarca.edu.co>
2026-07-23 10:15:17 -0500

    Agrega botón de

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("hola mundo")
+
+
+
+const boton = document.getElementById("botonTodas");
+
+boton.addEventListener("click", function () {
+    alert("¡Hola! Presionaste el botón.");
+});
