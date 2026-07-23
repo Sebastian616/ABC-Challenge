c315a12a73d9a51a3c6f866909475deb60105210
SamuelVelez_G <samu2005vel@gmail.com>
2026-07-23 10:41:51 -0500

    Contador.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("hola mundo")
+let letrasVistas = 0;
+
+function voltear(card) {
+  card.classList.toggle('volteada');
+
+  // Solo sumamos si la card se est volteando por primera vez
+  if (card.classList.contains('volteada') && !card.dataset.vista) {
+    letrasVistas++;
+    card.dataset.vista = 'true'; // marcamos esta card como ya vista
+    document.getElementById('contador-letras').textContent = letrasVistas;
+  }
+}
